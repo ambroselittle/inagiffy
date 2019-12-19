4c218ae2404653649865040d9874cacfad021e6d
Ambrose Little <2660021+ambroselittle@users.noreply.github.com>
2019-12-19 12:52:35 -0500

    Add no matches message.

--- src/components/InfiniteList.jsx
+++ src/components/InfiniteList.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Loader } from 'semantic-ui-react';
+import { Header, Loader } from 'semantic-ui-react';
 
 export const InfiniteList = ({ items, renderItem, isLoading = true }) => {
 
-    return (isLoading ? <Loader active inline='centered' /> :
-        <div>
-            {items && items.map(item => renderItem(item))}
-        </div>
-    )
-
+    return (isLoading ? <Loader active inline='centered' /> : (
+        items && items.length > 0 ?
+            <div>
+                {items && items.map(item => renderItem(item))}
+            </div>
+            : <Header>No matching giphies. Try again!</Header>
+    ));
 }
