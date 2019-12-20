c0fa93df2a73ce8f3b050fd12ca76ca56882edc2
Ambrose Little <2660021+ambroselittle@users.noreply.github.com>
2019-12-20 12:30:31 -0500

    Updates

---
+++ precache-manifest.88aaea5e1864a7c0d1be0958b45d28d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b5b985c19d0b0503b78c5c1f738576",
+    "url": "/inagiffy/index.html"
+  },
+  {
+    "revision": "ca8bb6a86617451de4fd",
+    "url": "/inagiffy/static/css/main.fd29f3c0.chunk.css"
+  },
+  {
+    "revision": "1b776fe3a740a9727fe6",
+    "url": "/inagiffy/static/js/2.67a122d9.chunk.js"
+  },
+  {
+    "revision": "f8513fb9f2981a1b206fb733b985f272",
+    "url": "/inagiffy/static/js/2.67a122d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ca8bb6a86617451de4fd",
+    "url": "/inagiffy/static/js/main.66a677f4.chunk.js"
+  },
+  {
+    "revision": "aa97e5feee6ec95f001a",
+    "url": "/inagiffy/static/js/runtime-main.8b423331.js"
+  }
+]);
